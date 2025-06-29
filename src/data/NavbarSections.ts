9d0cfd99094db4a643eedf32b1f0b331b716332c
PaulBaFi <paulalexisbarahonafigueroa@gmail.com>
2025-06-29 03:15:58 -0500

    feat: creacion del repositorio

---
+++ src/data/NavbarSections.ts
@@ -0,0 +1,50 @@
+export const relacionesComercialesSections = [
+  { id: "persona", label: "Personas", href: "/panel/personas" },
+  { id: "empresa", label: "Empresas", href: "/panel/empresas" },
+];
+
+export const recursosHumanosSections = [
+  { id: "empleados", label: "Empleados", href: "/panel/empleados" },
+  { id: "cargos", label: "Cargos", href: "/panel/cargos" },
+  { id: "contratos", label: "Contratos", href: "/panel/contratos" },
+];
+
+export const gestionUsuariosSections = [
+  { id: "usuarios", label: "Usuarios", href: "/panel/usuarios" },
+  { id: "roles", label: "Roles", href: "/panel/roles" },
+];
+
+export const gestionInventariosSections = [
+  { id: "categorias", label: "Categorías", href: "/panel/categorias" },
+  { id: "productos", label: "Productos", href: "/panel/productos" },
+  { id: "almacenes", label: "Almacenes", href: "/panel/almacenes" },
+];
+
+export const gestionPedidosSections = [
+  { id: "proveedores", label: "Proveedores", href: "/panel/proveedores" },
+  { id: "pedidos", label: "Pedidos", href: "/panel/pedidos" },
+];
+
+export const gestionVentasSections = [
+  { id: "ventas", label: "Ventas", href: "/panel/ventas" },
+  { id: "clientes", label: "Clientes", href: "/panel/clientes" },
+];
+
+export const configuracionesSections = [
+  { id: "nosotros", label: "Nosotros", href: "/panel/nosotros" },
+  { id: "perfil", label: "Perfil de usuario", href: "/panel/perfil" },
+];
+
+export const reportesSections = [
+  {
+    id: "reportes-ventas",
+    label: "Reportes de ventas",
+    href: "/panel/reportes-ventas",
+  },
+  {
+    id: "reportes-pedidos",
+    label: "Reportes de pedidos",
+    href: "/panel/reportes-pedidos",
+  },
+  { id: "kardex", label: "Kardex", href: "/panel/kardex" },
+];
